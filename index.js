1474fa234f7e8a6ac5eef154dc6a8c841ef228a1
mwoodpatrick <mwoodpatrick@gmail.com>
2022-12-29 11:46:56 -0800

    updated index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var count = 0;
 
 app.get("/", async (req, res) => {
   count++;
-  res.json({ status: `OK: ${count} updated` });
-  console.log(`response ${count}`);
+  res.json({ status: `OK: ${count} updated 2` });
+  console.log(`response ${count} updated`);
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port} updated`);
 });
